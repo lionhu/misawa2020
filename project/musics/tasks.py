fa2d515a26968ee4a2e258a54c93b1483e0b3e27
lionhu <huhaiguang@me.com>
2019-12-19 23:15:50 +0900

    change fetch bank rate task

--- project/musics/tasks.py
+++ project/musics/tasks.py
@@ -64,7 +64,7 @@ def sendEmail(email="huhaiguang@me.com"):
 
 
 @shared_task
-def fetch_bankrate():
+def fetch_bankrate(toUser):
     subject = 'message from chat'
     r = ShowapiRequest("http://route.showapi.com/105-32","76812","ff6b110b4d884acf9d1ddc030a16c5a0" )
     r.addBodyPara("bankCode", "icbc")
@@ -108,62 +108,9 @@ def fetch_bankrate():
         logger.error(cache.get("todayrate"))
 
     html_content = render_to_string('emails/todayrate.html',{'todayrate':jpy_rate})
-    msg = EmailMessage("[Exrate Notification]Today's rate",html_content,settings.DEFAULT_FROM_EMAIL,["lionhu2009@gmail.com"])
+    msg = EmailMessage("[Exrate Notification]Today's rate",html_content,settings.DEFAULT_FROM_EMAIL,[toUser])
     msg.content_subtype = "html" # Main content is now text/html
     msg.send()
-
-
-@shared_task
-def NotificationMemberTodayRate(toUser):
-
-    logger.error(toUser)
-    # subject = 'message from chat'
-    # r = ShowapiRequest("http://route.showapi.com/105-32","76812","ff6b110b4d884acf9d1ddc030a16c5a0" )
-    # r.addBodyPara("bankCode", "icbc")
-    # res = r.post()
-
-    # data=json.loads(res.text)
-
-    # fetch_error = data["showapi_res_error"]
-
-    # if not fetch_error:
-    #   rates=data["showapi_res_body"]["codeList"]
-    #   data_time=data["showapi_res_body"]["time"]
-    #   data_day=data["showapi_res_body"]["day"]
-    #   l_names = [d.get("name") for d in rates]
-
-    #   jpy_index = l_names.index("日元")
-
-    #   if jpy_index >-1:
-    #     jpy_rate=rates[jpy_index]
-    #     jpy_rate_db = {
-    #         "name":"jpy",
-    #         "code":jpy_rate["code"],
-    #         "hui_in":decimal.Decimal(jpy_rate["hui_in"]),
-    #         "hui_out":decimal.Decimal(jpy_rate["hui_out"]),
-    #         "chao_in":decimal.Decimal(jpy_rate["chao_in"]),
-    #         "chao_out":decimal.Decimal(jpy_rate["chao_out"])
-    #       }
-    #     jpy_rate = {
-    #         "name":"jpy",
-    #         "time":data_time,
-    #         "day":data_day,
-    #         "code":jpy_rate["code"],
-    #         "hui_in":decimal.Decimal(jpy_rate["hui_in"]),
-    #         "hui_out":decimal.Decimal(jpy_rate["hui_out"]),
-    #         "chao_in":decimal.Decimal(jpy_rate["chao_in"]),
-    #         "chao_out":decimal.Decimal(jpy_rate["chao_out"])
-    #       }
-    #     BankRate.objects.create(**jpy_rate_db)
-
-    #     cache.set("todayrate",jpy_rate,3600)
-    #     logger.error(cache.get("todayrate"))
-
-    # html_content = render_to_string('emails/todayrate.html',{'todayrate':jpy_rate})
-    # msg = EmailMessage("[Exrate Notification]Today's BOC rate",html_content,settings.DEFAULT_FROM_EMAIL,["crs@nichiei.service"])
-    # msg.content_subtype = "html"
-    # msg.send()
-
 
 
 @shared_task
